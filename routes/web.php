242fbd3e152880ea034a151021fa745bf6d50478
toscayconnery <toscayoelconnery@gmail.com>
2017-12-02 13:32:46 +0700

    merge

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,7 @@ Route::get('/daftarmentor', function(){
   return view('dashboard/daftarmentor');
 });
 
-<<<<<<< HEAD
-=======
+
 // Login
 //Route::post('/login', 'UserController@login')->name('user.login');
 
@@ -32,19 +31,15 @@ Route::get('/daftarmentor', function(){
 Route::post('/register', 'UserController@registerUser')->name('user.register');
 
 Route::post('/biodata', 'BiodataController@setBiodata')->name('biodata.set');
->>>>>>> 5fe1492bd04ae58a9ca3fa787f6955c54c5788f1
 
 Route::get('/daftarbimbingan', function(){
   return view('dashboard/daftarbimbingan');
 });
 
-<<<<<<< HEAD
 Route::get('/datakelompok', function(){
   return view('dashboard/datakelompok');
 });
-=======
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> 5fe1492bd04ae58a9ca3fa787f6955c54c5788f1
