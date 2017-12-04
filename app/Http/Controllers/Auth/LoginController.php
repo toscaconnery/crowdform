ed2aed2f08f251e725153e784f3441560efc19d3
toscayconnery <toscayoelconnery@gmail.com>
2017-12-04 15:54:03 +0700

    login redirect to..

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/dashboardhome';
 
     /**
      * Create a new controller instance.
